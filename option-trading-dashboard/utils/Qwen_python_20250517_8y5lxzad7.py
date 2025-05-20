c06d1ab3ed0a68e327c5b0de2cacb787a88c0fa2
iatin74 <iatin@yahoo.de>
2025-05-20 13:55:14 +0200

    Delete option-trading-dashboard/utils directory

--- option-trading-dashboard/utils/Qwen_python_20250517_8y5lxzad7.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def simulate_covered_call(call_df, stock_price, shares=100):
-    call_df = call_df[['strike', 'ask']].copy()
-    call_df['break_even'] = stock_price - call_df['ask']
-    call_df['max_profit'] = (call_df['strike'] - stock_price) + call_df['ask']
-    call_df['breakeven_move'] = ((stock_price - call_df['break_even']) / stock_price) * 100
-    return call_df.sort_values(by='strike')
