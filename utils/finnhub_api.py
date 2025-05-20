0ec52bea917aedf381c4b0f08531588678e4a310
iatin74 <iatin@yahoo.de>
2025-05-20 21:59:40 +0200

    Update finnhub_api.py

--- utils/finnhub_api.py
+++ utils/finnhub_api.py
@@ -26,3 +26,23 @@ def get_options_chain(symbol):
         print(f"Fehler beim Abrufen der Daten: {response.status_code}")
         print(response.text)
         return None
+        import requests
+from config import FINNHUB_API_KEY
+
+def get_options_chain(symbol):
+    url = f"https://finnhub.io/api/v1/stock/option-chain?symbol= {symbol}&token={FINNHUB_API_KEY}"
+    
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            return data
+        except Exception as e:
+            print(f"Fehler beim Parsen der JSON-Antwort: {e}")
+            print(f"Antwort-Inhalt: {response.text}")
+            return None
+    else:
+        print(f"Fehler beim Abrufen der Daten: {response.status_code}")
+        print(response.text)
+        return None
