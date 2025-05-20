493361c6d7b10d7105effddb15608c03c3c387e4
iatin74 <iatin@yahoo.de>
2025-05-20 17:09:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TRADIER_API_KEY = "4nQBreuudSUdOdu2g0XzCeBwWRD7"
 TELEGRAM_BOT_TOKEN = ""  # Optional: Telegram Bot Token
 TELEGRAM_CHAT_ID = ""    # Optional: Telegram Chat ID
+FINNHUB_API_KEY = "d0m9i21r01qkesvjdu7gd0m9i21r01qkesvjdu80"
