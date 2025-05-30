63bf2773e6a3a645d4880349123073a41e882c3d
iatin74 <iatin@yahoo.de>
2025-05-30 09:53:46 +0200

    Update tradier_api.py

--- utils/tradier_api.py
+++ utils/tradier_api.py
@@ -18,20 +18,14 @@ def get_options_chain(symbol):
     response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
-        try:
-            data = response.json()
-            expirations = sorted(set(option['expiration_date'] for option in data['options']['option']))
-            data['expirations'] = expirations
-            return data
-        except Exception as e:
-            print(f"Fehler beim Parsen der JSON-Antwort: {e}")
-            print(f"Antwort-Inhalt: {response.text}")
-            return None
+        data = response.json()
+        expirations = sorted(set(option['expiration_date'] for option in data['options']['option']))
+        data['expirations'] = expirations
+        return data
     else:
         print(f"Fehler beim Abrufen der Daten: {response.status_code}")
         print(response.text)
         return None
-        import requests
 from config import TRADIER_API_KEY
 
 BASE_URL = "https://sandbox.tradier.com/v1/markets "
