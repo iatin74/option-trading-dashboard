cdb0b95ae753aa549e6b47e96d29919177775f19
iatin74 <iatin@yahoo.de>
2025-05-30 10:27:19 +0200

    Update yahoo_finance_api.py

--- utils/yahoo_finance_api.py
+++ utils/yahoo_finance_api.py
@@ -1,1 +1,31 @@
+# utils/yahoo_finance_api.py
 
+import yfinance as yf
+
+def get_options_chain(symbol):
+    ticker = yf.Ticker(symbol)
+    options = ticker.options  # Liste aller Expirations
+
+    if not options:
+        return None
+
+    all_data = []
+    for expiry in options:
+        calls = ticker.option_chain(expiry).calls
+        puts = ticker.option_chain(expiry).puts
+        calls['expiration_date'] = expiry
+        puts['expiration_date'] = expiry
+        calls['option_type'] = 'call'
+        puts['option_type'] = 'put'
+        all_data.append(calls)
+        all_data.append(puts)
+
+    full_df = pd.concat(all_data)
+    full_df['underlying_price'] = ticker.history(period="1d")["Close"][0]
+
+    return {
+        "options": {
+            "option": full_df.to_dict(orient="records")
+        },
+        "expirations": options
+    }
